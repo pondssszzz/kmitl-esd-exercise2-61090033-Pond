69b4d0f96c9b65af4eaa1ca423e386a9c46cde5b
pondssszzz <61540953+pondssszzz@users.noreply.github.com>
2021-02-25 05:30:46 +0700

    Update CustomerController.java

--- server/src/main/java/th/kmitl/esd/api/CustomerController.java
+++ server/src/main/java/th/kmitl/esd/api/CustomerController.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 import kmitl.esd.exercise2.model.CustomerDTO;
 import java.util.List;
+import kmitl.esd.exercise.service.CustomerService;
 /**
  * Web service for customer
  * Produce endpoints as a part of the server API
@@ -15,24 +16,37 @@ import java.util.List;
 @RequestMapping("/customer")
 public class CustomerController {
 
-    private final AtomicLong counter = new AtomicLong();
-    private static List<CustomerDTO> customers = new ArrayList<>();
-
-    static {
-        CustomerDTO customerTom = new CustomerDTO(1l,"Jerry", 15);
-        customers.add(customerTom);
-
-    }
-
     /**
      * Function to get all of the customers
      * Can be called using GET method
      *
-     * @return list of all customers
+     * @return list of customers
      */
-    @GetMapping("/findall")
-    public List<CustomerDTO> getAllCustomer() {
-        return customers; //ToDo provide a list of customer
+    @GetMapping
+    public List<CustomerDTO> getAllCustomers() {
+        return CustomerService.INSTANCE.getAllCustomers();
+    }
+
+    /**
+     * Function to create customer
+     * Can be called using POST method
+     *
+     * @return customer
+     */
+    @PostMapping
+    public CustomerDTO createCustomer(@RequestBody CustomerDTO customer) {
+        return CustomerService.INSTANCE.createCustomer(customer);
+    }
+
+    /**
+     * Function to update customer
+     * Can be called using PUT method
+     *
+     * @return customer
+     */
+    @PutMapping
+    public CustomerDTO updateCustomer(@RequestBody CustomerDTO customer) {
+        return CustomerService.INSTANCE.updateCustomer(customer);
     }
 
     /**
@@ -43,43 +57,7 @@ public class CustomerController {
      * @return boolean - the result of deletion
      */
     @DeleteMapping("/{id}")
-    public Boolean deleteCustomer(@PathVariable("id") Long customerId) {
-        CustomerDTO customerToBeDeleted = customers.stream().filter(c -> c.getId().equals(customerId)).findFirst().get();
-        if (customerToBeDeleted != null) {
-            customers.remove(customerToBeDeleted);
-            return true;
-        } else {
-            return false;
-        }
-    }
-    /**
-     * Function to create customer
-     * Can be called using POST method
-     *
-     * @return customer
-     */
-
-    @PostMapping
-    public CustomerDTO createCustomer(CustomerDTO customer) {
-        customers.add(customer);
-        return customer;
-    }
-    /**
-     * Function to update customer
-     * Can be called using PUT method
-     *
-     * @return customer
-     */
-    @PutMapping
-    public CustomerDTO updateCustomer(CustomerDTO customer) {
-        CustomerDTO customerToBeUpdated = customers.stream().filter(c -> c.getId().equals(customer.getId())).findFirst().get();
-        if (customerToBeUpdated != null){
-            customers.remove(customerToBeUpdated);
-
-        } else{
-            return null;
-        }
-        customers.add(customer);
-        return customer;
+    public boolean deleteCustomer(@PathVariable("id") Long customerId) {
+        return CustomerService.INSTANCE.deleteCustomer(customerId);
     }
 }
